Fixed sign of constant jacobian terms

boltzmann/protein/internal.py
@@ -119,7 +119,7 @@ class InternalCoordinateTransform(transforms.Transform):
             self._fix_dih(transformed)
             self._setup_std_dih(transformed)
             transformed[:, self.dih_indices] /= self.std_dih
-            scale_jac = (
+            scale_jac = -(
                 torch.sum(torch.log(self.std_bonds))
                 + torch.sum(torch.log(self.std_angles))
                 + torch.sum(torch.log(self.std_dih))

boltzmann/protein/pca.py
@@ -70,7 +70,7 @@ class PCATransform(nn.Module):
             V = V[:, :keep_dims]
 
             # Store the jacobian for later.
-            jac = torch.sum(torch.log(self.stds))
+            jac = -torch.sum(torch.log(self.stds))
             self.register_buffer("jac", jac)
 
             # Store the whitening / blackening matrices for later.